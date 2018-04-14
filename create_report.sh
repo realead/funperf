9f7acfe9e5ab39f1a05f5a418a240992d4d8b9ef
Egor Dranischnikow <egor.dranischnikow@googlemail.com>
2018-04-14 23:07:19 +0200

    adding more functions

--- create_report.sh
+++ create_report.sh
@@ -1,9 +1,10 @@
+set -e
 
 RESULT="../report/test_results.txt"
 
 
 cd src
-g++ tester.cpp filler.cpp -O3 -std=c++11 -I../timeitcpp/include -o tester
+g++ tester.cpp filler.cpp -O3 -std=c++11 -I../timeitcpp/include -o tester 
 ./tester > $RESULT
 
 
--- src/tester.cpp
+++ src/tester.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <string>
 #include <iostream>
 #include <cmath>
 
@@ -37,29 +38,49 @@ struct Transform{
 };
 
 
-//it is small enough for L1-cache
-const int N=2000;
+
+
+double MULT_TIME=0.0;
+
+
 const double MAX_OMEGA=1e20;
 const double MIN_OMEGA=1e-20;
 const double STEP_FACTOR=1.4;
 
+template<typename Trafo>
+void sample_with_timeit(const std::string &fun_name, std::vector<double> &vec){
+   for(double omega=MIN_OMEGA; omega<MAX_OMEGA; omega*=STEP_FACTOR){
+      double time=timeitmagic::timeit(Trafo(vec), Fill(vec, omega), 100).time;
+      std::cout<<fun_name<<","<<omega<<","<<(time/MULT_TIME)<<"\n";
+   } 
+}
+
+
+//it is small enough for L1-cache
+const int N=2000;
+
+
 int main(){
-   
+ 
    std::vector<double> vec(N);
 
    //repeat it 20 times as warm up:
-   double mult_time=timeitmagic::timeit([&vec](){for(auto &v:vec) v*=1.1;}, Fill(vec, 1.0),20).time;
-   std::cerr<<"Mult time:"<<mult_time<<"\n";
+   MULT_TIME=timeitmagic::timeit([&vec](){for(auto &v:vec) v*=1.1;}, Fill(vec, 1.0),20).time;
+   std::cerr<<"Mult time:"<<MULT_TIME<<"\n";
    std::cerr<<"Dep time: "<<timeitmagic::timeit([&vec](){double prod=1.0; for(auto &v:vec) prod*=v; vec[0]=prod;}, Fill(vec, 1.0),20).time<<"\n";
 
    //header: 
    std::cout<<"fun_name,argument,flops\n";
 
-   //functions:
-   for(double omega=MIN_OMEGA; omega<MAX_OMEGA; omega*=STEP_FACTOR){
-      double time=timeitmagic::timeit(Transform<&sin>(vec), Fill(vec, omega)).time;
-      std::cout<<"sin,"<<omega<<","<<(time/mult_time)<<"\n";
-   } 
+   //trig functions:
+   sample_with_timeit<Transform<&sin> >("sin", vec);
+   sample_with_timeit<Transform<&cos> >("cos", vec);
+   sample_with_timeit<Transform<&tan> >("tan", vec);
+
+   //a-trig functions:
+   sample_with_timeit<Transform<&asin> >("asin", vec);
+   sample_with_timeit<Transform<&acos> >("acos", vec);
+   sample_with_timeit<Transform<&atan> >("atan", vec);
 
    //ensure that the last was not optimized away:
    fill(1.0, vec);
